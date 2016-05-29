034480de1c5e9e25ee08ac38fcccf9d049e46587
tekitonayatu <tekitonayatu@gmail.com>
2016-05-29 23:21:33 +0900

    新規登録

---
+++ google Extention/myscript.js
@@ -0,0 +1,10 @@
+$(function() {
+	$(document).on("DOMNodeInserted", function(e) {
+		var target = e.target;
+		$(target).find('.videoAdUiSkipButton').each(function(){
+			setTimeout(function(){
+				$('.videoAdUiSkipButton').trigger("click");
+			},8000);
+		});
+	});
+});
